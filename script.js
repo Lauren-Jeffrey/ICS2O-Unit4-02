38ea1b6a7c8fc74dab604932d64d0a44de50bd1a
LaurenJeffrey <lauren.jeffrey@ocsbstudent.ca>
2020-12-14 15:47:16 +0000

    js edited

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function myFunction () {
-  let x = document.getElementById("hideandshowtext");
-  if (x.style.display === "none") {
-    x.style.display = "block";
+  const x = document.getElementById('hideandshowtext')
+  if (x.style.display === 'none') {
+    x.style.display = 'block'
   } else {
-    x.style.display = "none";
+    x.style.display = 'none'
   }
 }
 function displayPhrase () {
-    document.getElementById("changetext").innerHTML = 'text changed!';
+  document.getElementById('changetext').innerHTML = 'text changed!'
 }
 function hideshowimage () {
-  let x = document.getElementById("hideandshowimage")
-  if (x.style.display === "none") {
-    x.style.display = "block"
+  const x = document.getElementById('hideandshowimage')
+  if (x.style.display === 'none') {
+    x.style.display = 'block'
   } else {
-    x.style.display = "none"
+    x.style.display = 'none'
   }
 }
