44c405da5a5f6ce06d4779d207f2436b9f9a32b5
LaurenJeffrey <lauren.jeffrey@ocsbstudent.ca>
2020-12-14 15:29:36 +0000

    fixed html and css

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function myFunction() {
-  var x = document.getElementById("hideandshowtext");
+  let x = document.getElementById("hideandshowtext");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   }
 }
-function displayPhrase()
-{
+function displayPhrase() {
     document.getElementById("changetext").innerHTML = 'text changed!';
 }
 function hideshowimage() {
